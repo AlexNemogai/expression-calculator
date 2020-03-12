a0ba886291b1e3404f56cc225dad78ec67bb7faf
Sasha <AlexandrNemogai@gmail.com>
2020-03-12 15:41:46 +0300

    init:expressionCalculator

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ function eval() {
     // Do not use eval!!!
     return;
 }
-
 function expressionCalculator(expr) {
-    // write your solution here
+    let bracketsCount = 0;
+    let strWithoutSpace = expr.split(' ').join('');
+    let masWithoutSpace = expr.split(' ').join('').split('');
+    for(let i = 0; i<masWithoutSpace.length; i++){
+        if(masWithoutSpace[i] == '(') bracketsCount++;
+        if(masWithoutSpace[i] == ')') bracketsCount--;
+        if(masWithoutSpace[i] == '/' && masWithoutSpace[i+1] == '0') throw new Error('TypeError: Division by zero.');
+    }
+    if(bracketsCount != 0) throw new Error ('ExpressionError: Brackets must be paired');
+    let returnCalculatedFunc = new Function(`return ${strWithoutSpace}`);
+    return returnCalculatedFunc();
 }
-
 module.exports = {
     expressionCalculator
 }
